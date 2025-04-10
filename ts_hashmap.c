0b74637649bedfcd40ddc119053ed63eacf818c1
ablaberge <annika.laberge@icloud.com>
2025-04-10 04:09:06 +0000

    delete and free added, not thread safe yet

--- ts_hashmap.c
+++ ts_hashmap.c
@@ -46,7 +46,7 @@ int get(ts_hashmap_t *map, int key)
     }
     entry = entry->next;
   }
-  return INT_MAX;  // Key not found
+  return INT_MAX; // Key not found
 }
 
 /**
@@ -60,37 +60,41 @@ int put(ts_hashmap_t *map, int key, int value)
 {
   int index = ((unsigned int)key) % map->capacity;
   ts_entry_t *entry = map->table[index];
- // Traverse the linked list
-    while (entry != NULL)
+  // Traverse the linked list
+  while (entry != NULL)
+  {
+    if (entry->key == key) // Key exists, replace the value
     {
-        if (entry->key == key) // Key exists, replace the value
-        {
-            int temp = entry->value;
-            entry->value = value;
-            map->numOps++;
-            return temp;
-        }
-        if (entry->next == NULL) { // Need this to handle case where list isn't empty
-          break;
-        }
-        entry = entry->next;
+      int temp = entry->value;
+      entry->value = value;
+      map->numOps++;
+      return temp;
     }
-
-    // Key not found, create a new entry
-    ts_entry_t *entry2 = malloc(sizeof(ts_entry_t));
-    entry2->key = key;
-    entry2->value = value;
-    entry2->next = NULL;
-
-    if (entry == NULL) {
-        map->table[index] = entry2; // Empty list, this is the first element 
-    } else {
-        entry->next = entry2; // We're adding to this linked list
+    if (entry->next == NULL)
+    { // Need this to handle case where list isn't empty
+      break;
     }
+    entry = entry->next;
+  }
 
-    map->size++;
-    map->numOps++;
-    return INT_MAX;
+  // Key not found, create a new entry
+  ts_entry_t *entry2 = malloc(sizeof(ts_entry_t));
+  entry2->key = key;
+  entry2->value = value;
+  entry2->next = NULL;
+
+  if (entry == NULL)
+  {
+    map->table[index] = entry2; // Empty list, this is the first element
+  }
+  else
+  {
+    entry->next = entry2; // We're adding to this linked list
+  }
+
+  map->size++;
+  map->numOps++;
+  return INT_MAX;
 }
 
 /**
@@ -101,7 +105,38 @@ int put(ts_hashmap_t *map, int key, int value)
  */
 int del(ts_hashmap_t *map, int key)
 {
-  // TODO
+
+  int index = ((unsigned int)key) % map->capacity;
+  ts_entry_t *entry = map->table[index];
+  ts_entry_t *prev = NULL;
+
+  while (entry != NULL)
+  {
+    if (entry->key == key) // Found the entry for deletion!
+    {
+      int temp = entry->value; 
+
+      if (prev == NULL) // First item in the list
+      {
+        map->table[index] = entry->next;
+      }
+      else // In the middle or end of the list
+      {
+        prev->next = entry->next;
+      }
+
+      free(entry);
+      map->size--;
+      map->numOps++;
+      return temp;
+    }
+
+    prev = entry;
+    entry = entry->next;
+  }
+
+ // Key not found
+  map->numOps++;
   return INT_MAX;
 }
 
@@ -131,7 +166,23 @@ void printmap(ts_hashmap_t *map)
  */
 void freeMap(ts_hashmap_t *map)
 {
-  // TODO: iterate through each list, free up all nodes
-  // TODO: free the hash table
+  // Free each linked list in the table
+  for (int i = 0; i < map->capacity; i++)
+  {
+    ts_entry_t *entry = map->table[i];
+    while (entry != NULL)
+    {
+      ts_entry_t *next = entry->next;
+      free(entry);
+      entry = next;
+    }
+  }
+
+  // Free the table array itself
+  free(map->table);
+
+  // Free the map struct
+  free(map);
+
   // TODO: destroy locks
 }
