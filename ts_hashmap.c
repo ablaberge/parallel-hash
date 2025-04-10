fbf0497574ba42a358b72385095a33336254362c
ablaberge <annika.laberge@icloud.com>
2025-04-10 03:30:30 +0000

    init, get, and put implemented but not thread safe yet

--- ts_hashmap.c
+++ ts_hashmap.c
@@ -6,25 +6,47 @@
 #include "ts_hashmap.h"
 
 /**
- * Creates a new thread-safe hashmap. 
+ * Creates a new thread-safe hashmap.
  *
  * @param capacity initial capacity of the hashmap.
  * @return a pointer to a new thread-safe hashmap.
  */
-ts_hashmap_t *initmap(int capacity) {
-  // TODO
-  return NULL;
+ts_hashmap_t *initmap(int capacity)
+{
+  ts_hashmap_t *map = malloc(sizeof(ts_hashmap_t));
+  map->table = malloc(sizeof(ts_entry_t *) * capacity);
+
+  // Initialize all lists to null
+  for (int i = 0; i < capacity; i++)
+  {
+    map->table[i] = NULL;
+  }
+
+  map->capacity = capacity;
+  map->size = 0;
+  map->numOps = 0;
+
+  return map;
 }
 
-/**
- * Obtains the value associated with the given key.
- * @param map a pointer to the map
- * @param key a key to search
- * @return the value associated with the given key, or INT_MAX if key not found
- */
-int get(ts_hashmap_t *map, int key) {
-  // TODO
-  return INT_MAX;
+int get(ts_hashmap_t *map, int key)
+{
+  // Calculate the index using the hash function
+  int index = ((unsigned int)key) % map->capacity;
+
+  // Start at the head of the linked list in this bucket
+  ts_entry_t *entry = map->table[index];
+
+  // Traverse the linked list
+  while (entry != NULL)
+  {
+    if (entry->key == key)
+    {
+      return entry->value;
+    }
+    entry = entry->next;
+  }
+  return INT_MAX;  // Key not found
 }
 
 /**
@@ -34,9 +56,41 @@ int get(ts_hashmap_t *map, int key) {
  * @param value a value
  * @return old associated value, or INT_MAX if the key was new
  */
-int put(ts_hashmap_t *map, int key, int value) {
-  // TODO
-  return INT_MAX;
+int put(ts_hashmap_t *map, int key, int value)
+{
+  int index = ((unsigned int)key) % map->capacity;
+  ts_entry_t *entry = map->table[index];
+ // Traverse the linked list
+    while (entry != NULL)
+    {
+        if (entry->key == key) // Key exists, replace the value
+        {
+            int temp = entry->value;
+            entry->value = value;
+            map->numOps++;
+            return temp;
+        }
+        if (entry->next == NULL) { // Need this to handle case where list isn't empty
+          break;
+        }
+        entry = entry->next;
+    }
+
+    // Key not found, create a new entry
+    ts_entry_t *entry2 = malloc(sizeof(ts_entry_t));
+    entry2->key = key;
+    entry2->value = value;
+    entry2->next = NULL;
+
+    if (entry == NULL) {
+        map->table[index] = entry2; // Empty list, this is the first element 
+    } else {
+        entry->next = entry2; // We're adding to this linked list
+    }
+
+    map->size++;
+    map->numOps++;
+    return INT_MAX;
 }
 
 /**
@@ -45,20 +99,23 @@ int put(ts_hashmap_t *map, int key, int value) {
  * @param key a key to search
  * @return the value associated with the given key, or INT_MAX if key not found
  */
-int del(ts_hashmap_t *map, int key) {
+int del(ts_hashmap_t *map, int key)
+{
   // TODO
   return INT_MAX;
 }
 
-
 /**
  * Prints the contents of the map (given)
  */
-void printmap(ts_hashmap_t *map) {
-  for (int i = 0; i < map->capacity; i++) {
+void printmap(ts_hashmap_t *map)
+{
+  for (int i = 0; i < map->capacity; i++)
+  {
     printf("[%d] -> ", i);
     ts_entry_t *entry = map->table[i];
-    while (entry != NULL) {
+    while (entry != NULL)
+    {
       printf("(%d,%d)", entry->key, entry->value);
       if (entry->next != NULL)
         printf(" -> ");
@@ -72,7 +129,8 @@ void printmap(ts_hashmap_t *map) {
  * Free up the space allocated for hashmap
  * @param map a pointer to the map
  */
-void freeMap(ts_hashmap_t *map) {
+void freeMap(ts_hashmap_t *map)
+{
   // TODO: iterate through each list, free up all nodes
   // TODO: free the hash table
   // TODO: destroy locks
